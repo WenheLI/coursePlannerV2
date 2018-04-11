a169e2bd6a0b78d9dd6aae8688067e35a2846b61
MAXEE998 <hl2752@nyu.edu>
2018-04-11 09:42:14 +0800

    Comments added

--- course.py
+++ course.py
@@ -6,24 +6,33 @@ from selenium.webdriver.support.ui import Select
 
 locations = ["NYU Shanghai"]
 
-ops = webdriver.ChromeOptions().add_argument(
-    '--user-agent=Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36')
-driver = webdriver.Chrome(chrome_options=ops)
+# set up webdriver
+options = webdriver.ChromeOptions().add_argument(
+    '--user-agent=Mozilla/5.0 (Windows NT 6.1; WOW64)'
+    ' AppleWebKit/537.36 (KHTML, like Gecko)'
+    ' Chrome/51.0.2704.103 Safari/537.36')
+driver = webdriver.Chrome("bin/chromedriver", chrome_options=options)
 
-driver.get("http://albert.nyu.edu/albert_index.html")
+base_url = "http://albert.nyu.edu/albert_index.html"
+driver.get(base_url)
+
+# click into public course search
 buttons = driver.find_elements_by_class_name("buttonLink")
-
 buttons[1].click()
 
-courses = driver.find_elements_by_xpath("//a[starts-with(@id, 'LINK') and @class='PSHYPERLINK']")
+# using xpath to get all subjects
+subjects = driver.find_elements_by_xpath("//a[starts-with(@id, 'LINK') and @class='PSHYPERLINK']")
 selector = Select(driver.find_element_by_id("NYU_CLS_WRK2_DESCR254$33$"))
 
 # for s in range(len(selector.options)):
-selector.select_by_index(1)
-sleep(1)
 selector.select_by_index(2)
-print(courses.__len__())
+
+selector = Select(driver.find_element_by_id("NYU_CLS_WRK2_DESCR254$33$"))
+selector.select_by_index(3)
+
+#sleep(10)
+
+print(subjects.__len__())
 
 driver.quit()
-
 
